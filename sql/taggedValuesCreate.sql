1470168e3320f147ebdabe004173db2ddd7392df
Jim McAndrew <jim@loc8.us>
2016-05-23 07:21:27 -0600

    Remove field mapping, allow other primary keys

--- sql/taggedValuesCreate.sql
+++ sql/taggedValuesCreate.sql
@@ -3,6 +3,5 @@ CREATE TABLE "tagged_values"
      "id"             TEXT,
      "translator"     TEXT,
      "geometry_types" TEXT,
-     "value_mapping"  TEXT,
-     "field_mapping"  TEXT
+     "value_mapping"  TEXT
   );
