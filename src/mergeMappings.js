98ce7182b0c558bd8fb3126924977bb2d861a2c1
Jim McAndrew <jim@loc8.us>
2016-05-24 12:44:41 -0600

    Connect mappings from values

--- src/mergeMappings.js
+++ src/mergeMappings.js
@@ -1,5 +1,7 @@
 var tools = require('jm-tools');
 
+
+
 module.exports = function (id, valueMappings, fieldMappings, origMappings) {
   var findOtherMapping = function (primaryKey, otherMappings) {
     return otherMappings.filter(function (row) {
@@ -7,9 +9,11 @@ module.exports = function (id, valueMappings, fieldMappings, origMappings) {
     })[0];
   };
   var finalMappings = fieldMappings.map(function (row) {
-    var matchedObjs = findOtherMapping(row[id], origMappings) || {};
-    var matchedValueMapping = findOtherMapping(row[id], valueMappings) || {};
-    return tools.mergeObjects(matchedObjs.translator, matchedObjs.value_mapping, matchedValueMapping, row);
+    var matchedObjs = tools.denullify(findOtherMapping(row[id], origMappings) || {});
+    var matchedValueMapping = tools.denullify(findOtherMapping(row[id], valueMappings) || {});
+    var matchedCleanValues = extractTags(matchedValueMapping, 'id');
+    return tools.mergeObjects(matchedObjs.translator, matchedObjs.value_mapping, matchedCleanValues, tools.denullify(row));
   });
+  process.exit(0);
   return finalMappings;
 };
