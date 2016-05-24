480a62fd189755fd51574b09403f2ca7335cbdd5
Jim McAndrew <jim@loc8.us>
2016-05-24 11:40:13 -0600

    Clean up / test code

--- src/mergeMappings.js
+++ src/mergeMappings.js
@@ -1,14 +1,15 @@
 var tools = require('jm-tools');
 
-module.exports = function (id, fieldMappings, otherMappings) {
-  var findOtherMapping = function (primaryKey) {
+module.exports = function (id, valueMappings, fieldMappings, origMappings) {
+  var findOtherMapping = function (primaryKey, otherMappings) {
     return otherMappings.filter(function (row) {
-      return row[id] === primaryKey;
+      return row['id'] === primaryKey;
     })[0];
   };
   var finalMappings = fieldMappings.map(function (row) {
-    var matchedObjs = findOtherMapping(row[id]) || {};
-    return tools.mergeObjects(matchedObjs.translator, matchedObjs.value_mapping, row);
+    var matchedObjs = findOtherMapping(row[id], origMappings) || {};
+    var matchedValueMapping = findOtherMapping(row[id], valueMappings) || {};
+    return tools.mergeObjects(matchedObjs.translator, matchedObjs.value_mapping, matchedValueMapping, row);
   });
   return finalMappings;
 };
