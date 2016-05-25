a9991107037f65d3beb44711dfeb02b8fdad177d
Jim McAndrew <jim@loc8.us>
2016-05-25 15:58:25 -0600

    Parse the JSON objects out

--- src/mergeMappings.js
+++ src/mergeMappings.js
@@ -1,6 +1,16 @@
 var tools = require('jm-tools');
 var extractTags = require('./extractTags');
 
+var parseJson = function (str) {
+  var returnValue = {};
+  try {
+    returnValue = typeof str === 'string' ? JSON.parse(str) : str;
+  } catch (e) {
+    returnValue = {};
+  }
+  return returnValue;
+};
+
 module.exports = function (id, valueMappings, fieldMappings, origMappings) {
   var findOtherMapping = function (primaryKey, otherMappings) {
     return otherMappings.filter(function (row) {
@@ -11,7 +21,7 @@ module.exports = function (id, valueMappings, fieldMappings, origMappings) {
     var matchedObjs = tools.denullify(findOtherMapping(row[id], origMappings) || {});
     var matchedValueMapping = tools.denullify(findOtherMapping(row[id], valueMappings) || {});
     var matchedCleanValues = extractTags(matchedValueMapping, 'id');
-    var merged = tools.mergeObjects(matchedObjs.translator, matchedObjs.preset_mapping, matchedCleanValues, tools.denullify(row));
+    var merged = tools.mergeObjects(parseJson(matchedObjs.translator), parseJson(matchedObjs.preset_mapping), matchedCleanValues, tools.denullify(row));
     // Remove ids
     delete merged.id;
     delete merged[id];
