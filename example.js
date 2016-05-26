5c9917e965cb94e1785aa5dcb60228cee585f982
Jim McAndrew <jim@loc8.us>
2016-05-26 08:24:47 -0600

    Add example for parking lots

--- example.js
+++ example.js
@@ -5,7 +5,10 @@ var test = require('./');
 var geojsonSource = {
   'name': 'Input data',
   'connection': {
-    'data': fs.readFileSync(path.join(__dirname, '/test/data/exampleRoads.geojson'), 'UTF8'),
+    // 'data': fs.readFileSync(path.join(__dirname, '/test/data/exampleCampground.geojson'), 'UTF8'), // poi
+    'data': fs.readFileSync(path.join(__dirname, '/test/data/exampleRoads.geojson'), 'UTF8'), // roads
+    // 'data': fs.readFileSync(path.join(__dirname, '/test/data/exampleParkingLots.geojson'), 'UTF8'), //parkinglots
+    'translationType': 'roads', // poi trails parkinglots
     'type': 'geojson'
   },
   fields: {
@@ -13,7 +16,7 @@ var geojsonSource = {
   }
 };
 
-test(geojsonSource, 'roads').then(function (r) {
+test(geojsonSource).then(function (r) {
   console.log(JSON.stringify(JSON.parse(r), null, 2));
 }).catch(function (e) {
   throw e;
