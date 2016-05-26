a147b1abdc10d845ce3cbf4802a093a1498dad25
Jim McAndrew <jim@loc8.us>
2016-05-26 11:18:16 -0600

    remove case sensitivity for transltor geom types

--- sql/addPresetSettings.sql
+++ sql/addPresetSettings.sql
@@ -6,10 +6,10 @@ SET "preset_mapping" = (
       SELECT
         "Preset_Classes"."tags",
         CASE
-          WHEN "Preset_Classes"."type" =  "{{sourceName}}"."{{valueField}}" THEN 1 -- If the requested term equals the type, then rank it as 1
-          WHEN instr("Preset_Classes"."altNames", '"' ||   "{{sourceName}}"."{{valueField}}" || '"') THEN
-            length(substr("Preset_Classes"."altNames", 0, instr("Preset_Classes"."altNames", '"' ||   "{{sourceName}}"."{{valueField}}" || '"'))) - 
-            length(replace(substr("Preset_Classes"."altNames", 0, instr("Preset_Classes"."altNames", '"' ||   "{{sourceName}}"."{{valueField}}" || '"')), ',', '')) + 
+          WHEN lower("Preset_Classes"."type") =  lower("{{sourceName}}"."{{valueField}}") THEN 1 -- If the requested term equals the type, then rank it as 1
+          WHEN instr(lower("Preset_Classes"."altNames"), '"' || lower("{{sourceName}}"."{{valueField}}") || '"') THEN
+            length(substr(lower("Preset_Classes"."altNames"), 0, instr(lower("Preset_Classes"."altNames"), '"' || lower("{{sourceName}}"."{{valueField}}") || '"'))) - 
+            length(replace(substr(lower("Preset_Classes"."altNames"), 0, instr(lower("Preset_Classes"."altNames"), '"' ||  lower("{{sourceName}}"."{{valueField}}") || '"')), ',', '')) + 
             2
            ELSE null -- If it's in the altNames fields, then the rank is the position in that list plus 2
                     -- The first index in this list will be 0, but our ranks start at one
@@ -18,9 +18,9 @@ SET "preset_mapping" = (
         FROM "Preset_Classes"
         WHERE
           (
-            "point" = CASE WHEN instr("tagged_values"."geometry_types",'"Point"') THEN 'x' ELSE 'no match' END OR
-            "line" = CASE WHEN instr("tagged_values"."geometry_types",'"Polyline"') THEN 'x' ELSE 'no match' END OR
-            "polygon" = CASE WHEN instr("tagged_values"."geometry_types",'"Polygon"') THEN 'x' ELSE 'no match' END
+            "point" = CASE WHEN instr(lower("tagged_values"."geometry_types"),'"point"') THEN 'x' ELSE 'no match' END OR
+            "line" = CASE WHEN instr(lower("tagged_values"."geometry_types"),'"polyline"') THEN 'x' ELSE 'no match' END OR
+            "polygon" = CASE WHEN instr(lower("tagged_values"."geometry_types"),'"polygon"') THEN 'x' ELSE 'no match' END
           ) AND
           "rank" IS NOT NULL ORDER BY "rank" LIMIT 1
     ) AS "best_match") as "tags"
