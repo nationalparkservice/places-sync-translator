03dd105d71868072d821d4edb76c96581ee58993
Jim McAndrew <jim@loc8.us>
2016-06-08 13:10:33 -0600

    Deal with metadata columns

--- src/toGeojson.js
+++ src/toGeojson.js
@@ -13,19 +13,19 @@ module.exports = function (tableJson) {
 
 var createFeature = function (row) {
   row = JSON.parse(JSON.stringify(row));
-  var metadataColumns = ['id'];
-  var geometryColumns = ['geometry'];
+  var metadataColumn = 'id';
+  var geometryColumn = 'geometry';
   var feature = {
     'type': 'Feature',
-    'geometry': JSON.parse(row.geometry),
-    'metadata': {},
+    'geometry': JSON.parse(row[geometryColumn]),
+    'osmId': undefined,
+    'osmVersion': undefined,
+    'foreignKey': row[metadataColumn],
     'properties': {}
   };
   for (var property in row) {
-    if (metadataColumns.indexOf(property) === -1 && geometryColumns.indexOf(property) === -1) {
+    if (property !== metadataColumn && property !== geometryColumn) {
       feature.properties[property] = row[property];
-    } else if (metadataColumns.indexOf(property) !== -1) {
-      feature.metadata[property] = row[property];
     }
   }
   return feature;
