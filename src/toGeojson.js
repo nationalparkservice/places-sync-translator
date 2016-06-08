f641a7d72b2c0da9df8ace32aee124d3ec46479e
Jim McAndrew <jim@loc8.us>
2016-06-08 08:16:37 -0600

    Use the 'id' field for metadata

--- src/toGeojson.js
+++ src/toGeojson.js
@@ -13,14 +13,19 @@ module.exports = function (tableJson) {
 
 var createFeature = function (row) {
   row = JSON.parse(JSON.stringify(row));
+  var metadataColumns = ['id'];
+  var geometryColumns = ['geometry'];
   var feature = {
     'type': 'Feature',
     'geometry': JSON.parse(row.geometry),
+    'metadata': {},
     'properties': {}
   };
   for (var property in row) {
-    if (property !== 'geometry') {
+    if (metadataColumns.indexOf(property) === -1 && geometryColumns.indexOf(property) === -1) {
       feature.properties[property] = row[property];
+    } else if (metadataColumns.indexOf(property) !== -1) {
+      feature.metadata[property] = row[property];
     }
   }
   return feature;
